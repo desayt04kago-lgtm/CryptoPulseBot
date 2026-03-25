c9daa74fd12f58fad3d7f8ee338d1a98d4c4370b
Artem <desayt04kago@gmail.com>
2026-03-26 02:27:34 +0600

    функция: запись игровка в БД + фиксы и доработки

--- database.py
+++ database.py
@@ -1,13 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy import Integer, String, Column, Boolean
+from sqlalchemy import Integer, String, Boolean, Column
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
 
-engine = create_engine(f"postgresql+psycopg2://postgres:{os.getenv("database_password")}/@localhost/postgres")
+DB_PASSWORD = os.getenv('database_password')
+engine = create_engine(f"postgresql+psycopg2://postgres:{DB_PASSWORD}@localhost/postgres")
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
@@ -15,20 +15,33 @@ class Users(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     target = Column(String, nullable=True)
-    alerts = Column(Boolean, nullable=True)
-
+    alerts = Column(Boolean, nullable=False, default=True)
 Base.metadata.create_all(engine)
 
 def check_new_user(id: int) -> bool:
     """
-    returns True if user exists in DB\n
-    returns False if user does not exist in DB
+    returns False if user exists in DB\n
+    returns True if user does not exist in DB
+    :param id:
     """
     session = Session()
-    all_users = session.query().all()
+    all_users = session.query(Users).all()
     for user in all_users:
         if user.id == id:
             return False
     return True
+
+def register_new_user(id: int, target: str, alerts: bool) -> None:
+    """
+    adds user to DB\n
+    :param id:
+    :param target:
+    :param alerts:
+    """
+    session = Session()
+    user = Users(id=id, target=target, alerts=alerts)
+    session.add(user)
+    session.commit()
+    session.close()
 
 
